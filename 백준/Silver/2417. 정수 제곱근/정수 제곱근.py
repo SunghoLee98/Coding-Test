c7ecda9f3b3487c55ef445ccfc0d5aedb5566c8d
Sungho Lee <dltjdgh69@gmail.com>
2023-03-27 12:53:57 +0900

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,24 @@
+num = int(input())
+ans = int(num/2)
+st = 0
+ed = num
+
+if(num == 0) or (num==1) or (num==2):
+    print(num)
+else:
+    while(True):
+        
+        if((num==0) or (num==1) or (num==2)):
+            break
+        if(ans**2 >= num):
+            if((ans-1)**2 < num):
+                break
+            ed = ans
+            ans = int((st + ed) / 2)
+        else:
+            st = ans
+            ans = int((st + ed) / 2)
+        
+    print(ans)
+        
+    
