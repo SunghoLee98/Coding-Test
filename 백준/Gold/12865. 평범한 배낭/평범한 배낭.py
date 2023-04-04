25b515ba240665446f2ccc3f7b0fbcf85e60763b
Sungho Lee <dltjdgh69@gmail.com>
2023-04-04 20:27:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 4212 ms, Memory: 282468 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,21 @@
+str = input().split()
+
+n = int(str[0])
+maxW = int(str[1])
+
+dp = [[0] * (maxW+1)]
+for i in range(n):
+    tmp = input().split()
+    #bag.append([int(tmp[0]), int(tmp[1])])
+    wei = int(tmp[0])
+    val = int(tmp[1])
+    newdp = [0] * (maxW+1)
+    for j in range(1, maxW+1):
+        if (j-wei) < 0:
+            newdp[j] = dp[i][j]
+        else:
+            newdp[j] = max(dp[i][j - wei] + val, dp[i][j])
+
+    dp.append(newdp)
+
+print(dp[n][maxW])
